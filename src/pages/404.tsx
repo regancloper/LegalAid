5e4048a3e1b9911a87611259c8799d7d866c0789
Regan Loper <reganloper@Regans-MBP.attlocal.net>
2020-10-15 14:01:39 -0500

    styled 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Container } from 'react-bootstrap';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import PageHeader from '../components/PageHeader';
+import RoundedButton from '../components/RoundedButton';
 
 interface NotFoundProps {}
 
@@ -10,10 +13,14 @@ const NotFound: React.FC<NotFoundProps> = ({}) => {
 	return (
 		<Layout>
 			<SEO title="404: Not Found" />
-			<h1>Page Not Found</h1>
-			<p>
-				<Link to="/">Back to home</Link>
-			</p>
+			<Container>
+				<PageHeader text="Page Not Found" />
+				<div className="w-25 mx-auto" style={{ marginBottom: '200px' }}>
+					<Link to="/">
+						<RoundedButton text="Go Home" />
+					</Link>
+				</div>
+			</Container>
 		</Layout>
 	);
 };
